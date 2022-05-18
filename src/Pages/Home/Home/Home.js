07efe86486e7243a9952def794acab7f0361bbbe
FahadwebD <fahadchowdhury66779@gmail.com>
2022-05-18 20:25:03 +0600

    nav bar added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import useData from '../../../hooks/useData';
+import Navbar from '../../Shared/Navbar/Navbar';
 
 const Home = () => {
     const {apiData} = useData()
     console.log(apiData)
     return (
         <div>
-            
+            <Navbar></Navbar>
         </div>
     );
 };
